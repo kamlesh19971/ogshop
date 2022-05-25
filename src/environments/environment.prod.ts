b27903b01395e2b9edc857d9464dd463aae154da
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-25 18:22:59 +0530

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDoltPm0AonkN3D1N0mulF11bdoKMrOQW4",
+    authDomain: "ogshop-604ad.firebaseapp.com",
+    projectId: "ogshop-604ad",
+    storageBucket: "ogshop-604ad.appspot.com",
+    messagingSenderId: "160766355883",
+    appId: "1:160766355883:web:7683789970a8c834e78aee",
+    measurementId: "G-3F195BX634"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDoltPm0AonkN3D1N0mulF11bdoKMrOQW4",
+    authDomain: "ogshop-604ad.firebaseapp.com",
+    projectId: "ogshop-604ad",
+    storageBucket: "ogshop-604ad.appspot.com",
+    messagingSenderId: "160766355883",
+    appId: "1:160766355883:web:7683789970a8c834e78aee",
+    measurementId: "G-3F195BX634"
+  }
 };
 
 /*
