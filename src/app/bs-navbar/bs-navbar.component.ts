592708182041b80c70b694440a32bc3367908445
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-26 16:20:13 +0530

    logout added

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebaseAuth from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(x => console.log(x)
+    );
+  }
 
-  ngOnInit(): void {
+
+  logout() {
+    this.afAuth.signOut();
   }
 
 }
