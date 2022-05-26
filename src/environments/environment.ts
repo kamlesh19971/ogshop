0472e61bfc761dde53298fa5768f82d33672a04e
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-26 12:22:09 +0530

    firebase deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDoltPm0AonkN3D1N0mulF11bdoKMrOQW4",
-    authDomain: "ogshop-604ad.firebaseapp.com",
-    projectId: "ogshop-604ad",
-    storageBucket: "ogshop-604ad.appspot.com",
-    messagingSenderId: "160766355883",
-    appId: "1:160766355883:web:7683789970a8c834e78aee",
-    measurementId: "G-3F195BX634"
+    apiKey: "AIzaSyDN_d__vDtRc1QHpsj--g5MKg4XLkNBDXk",
+    authDomain: "ogshop-39f9d.firebaseapp.com",
+    projectId: "ogshop-39f9d",
+    storageBucket: "ogshop-39f9d.appspot.com",
+    messagingSenderId: "809204516276",
+    appId: "1:809204516276:web:400780c8610619896f5ee1",
+    measurementId: "G-E8MH742NZ4"
   }
 };
 
