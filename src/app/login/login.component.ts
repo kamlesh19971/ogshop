a2b175baef941c8eef51873ecddda6e3ca99b75d
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-26 15:43:32 +0530

    google login added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebaseAuth from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.afAuth.signInWithRedirect(new firebaseAuth.GoogleAuthProvider());
   }
 
 }
